a31e22146a90ed0cd0642fcb6e813a39a4364028
Linlin Yang <linlin@linlins-mbp.domain>
2021-12-13 11:28:26 -0800

    Modified job details

--- job-board/src/components/JobDetails.jsx
+++ job-board/src/components/JobDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import NavBar from './NavBar';
 import { useParams } from 'react-router';
@@ -8,12 +8,41 @@ const JobDetails = () => {
     const jobId = useParams().jobId;
 
     const findJobDetails = () => {
-        axios.get('http://localhost:8000/api/job/jobSearch')
+        axios.get('http://localhost:8000/api/job/jobSearch?id=' + jobId).then((response) => setJob(response[0])).catch((error) => console.log("Could not find Job."))
     }
+
+    const [job, setJob] = useState(null);
+    useEffect(findJobDetails, []);
+
+    const jobComponent = job ? 
+        (<div><div>
+            Job Title: {job.title}
+        </div>
+        <div>
+            Company Name: {job.companyName}
+        </div>
+        <div>
+            Location: {job.location}
+        </div>
+        <div>
+            Description: {job.description}
+        </div>
+        <div>
+            Employer Email Contact: {job.employerEmailContact}
+        </div>
+        <div>
+            Company Website: {job.companyWebsite ? job.companyWebsite : ""}
+        </div>
+        <div>
+            Posted Date: {job.postingDate}
+        </div>
+        </div>) :
+        (<div> No Job Found </div>);
+
     return (
         <div>
             <NavBar />
-            <div>Job Details</div>
+            <div>{jobComponent}</div>
         </div>
     )
 }
