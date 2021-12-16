2f453936de6a4fc5762fe03b8856c712109b2d25
Xiaolin Zhao <zxlnorth@gmail.com>
2021-12-16 15:47:03 -0800

    add job details page

--- job-board/src/components/JobDetails.jsx
+++ job-board/src/components/JobDetails.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useParams } from 'react-router';
 import { useLocation } from 'react-router-dom';
 import {useNavigate} from "react-router-dom";
+import '../styles/JobDetails.css';
 
 
 
@@ -14,30 +15,39 @@ const JobDetails = () => {
     console.log(job);
 
     const navigate = useNavigate();
-    const jobComponent = job ? 
-        (<div><div>
-            Job Title: {job.title}
+    const jobComponent = 
+        job 
+            ? 
+        <div className='jobContainer'>
+            <div>
+                <div className='jobDetail'>
+                    Job Title: {job.title}
+                </div>
+                <div className='jobDetail'> 
+                    Company Name: {job.companyName}
+                </div>
+                <div className='jobDetail'>
+                    Location: {job.location}
+                </div>
+                <div className='jobDetail'>
+                    Description: {job.description}
+                </div>
+                <div className='jobDetail'>
+                    Employer Email Contact: {job.employerEmailContact}
+                </div>
+                <div className='jobDetail'>
+                    Company Website: {job.companyWebsite ? job.companyWebsite : ""}
+                </div>
+                <div className='jobDetail'>
+                    Posted Date: {job.postingDate}
+                </div>
+            </div>
+            <div className='icons'>
+                icons(edit/delete/favorite)
+            </div>
         </div>
-        <div>
-            Company Name: {job.companyName}
-        </div>
-        <div>
-            Location: {job.location}
-        </div>
-        <div>
-            Description: {job.description}
-        </div>
-        <div>
-            Employer Email Contact: {job.employerEmailContact}
-        </div>
-        <div>
-            Company Website: {job.companyWebsite ? job.companyWebsite : ""}
-        </div>
-        <div>
-            Posted Date: {job.postingDate}
-        </div>
-        </div>) :
-        (<div> No Job Found </div>);
+            :
+        <div className='jobDetail'> No Job Found </div>;
     return (
         <div>
             <div>{jobComponent}</div>
